0c0122106d2d3d60f2cb33cc69137c50381586da
Tony Stark <www.tstark619@gmail.com>
2026-05-10 23:22:49 +0530

    Create SimpleMath.py
    Add exponentiation operator example

---
+++ SimpleMath.py
@@ -0,0 +1,8 @@
+# Day 2: Python's unique operators
+base = 5
+exponent = 3
+
+# In Python, ** means "to the power of"
+result = base ** exponent 
+
+print(f"{base} raised to the power of {exponent} is: {result}")
